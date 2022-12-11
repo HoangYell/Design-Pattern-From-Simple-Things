7431a2b27f24cc83bc0892770c8a01f2254b84c6
HoangYell <hoang.ngo@paradox.ai>
2022-12-11 15:08:42 +0700

    deploy

---
+++ bp2_strategy.py
@@ -0,0 +1,67 @@
+import random
+from abc import ABC, abstractmethod
+
+
+class Strategy(ABC):
+    name = ""
+
+    @abstractmethod
+    def pass_the_ball(self, formation):
+        pass
+
+
+class TikiTaka(Strategy):
+    name = "TikiTaka"
+
+    def pass_the_ball(self, formation):
+        defenders = formation[0]
+        midfielder_forwards = [*formation[1], *formation[2]]
+
+        random.shuffle(defenders)
+        random.shuffle(midfielder_forwards)
+        return [*defenders, *midfielder_forwards]
+
+
+class CounterAttack(Strategy):
+    name = "CounterAttack"
+
+    def pass_the_ball(self, formation):
+        defenders = random.choice(formation[0])
+        midfielders = random.choice(formation[1])
+        forwards = random.choice(formation[2])
+        return [defenders, midfielders, forwards]
+
+
+class FootballClub:
+    def __init__(self, strategy):
+        self._strategy = strategy
+
+    @property
+    def strategy(self):
+        return self._strategy
+
+    @strategy.setter
+    def strategy(self, strategy):
+        self._strategy = strategy
+
+    def pass_the_ball(self):
+        formation = [
+            ["LB", "CB", "CB", "CB"],
+            ["LCM", "CDM", "RCM"],
+            ["LW", "CF", "RW"],
+        ]
+        result = self._strategy.pass_the_ball(formation)
+        result.append("GOAAAALLL!!!")
+        print(" >> ".join(result))
+
+
+if __name__ == "__main__":
+    strategy = CounterAttack()
+    football_club = FootballClub(strategy)
+    print(f"Manager: Hey team! apply {strategy.name}")
+    football_club.pass_the_ball()
+
+    strategy = TikiTaka()
+    print(f"Manager: Hey team! apply {strategy.name}")
+    football_club.strategy = TikiTaka()
+    football_club.pass_the_ball()
