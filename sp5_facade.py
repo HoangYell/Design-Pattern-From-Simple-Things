e9bdf85802cb0d80c7479b0f4438e9ba73e691ee
HoangYell <hoang.ngo@paradox.ai>
2023-05-16 23:10:10 +0700

    deploy

---
+++ sp5_facade.py
@@ -0,0 +1,34 @@
+# Subsystem classes
+class Elevator:
+    def move(self, floor):
+        print(f"Moving to floor {floor}...")
+
+
+class Security:
+    def check(self, id):
+        print(f"Checking id {id}...")
+        return True
+
+
+class Reception:
+    def greet(self, name):
+        print(f"Hello {name}, welcome to our building!")
+
+
+# Facade class
+class BuildingFacade:
+    def __init__(self):
+        self.elevator = Elevator()
+        self.security = Security()
+        self.reception = Reception()
+
+    def enter(self, id, name, floor):
+        if self.security.check(id):
+            self.reception.greet(name)
+            self.elevator.move(floor)
+
+
+# Client code
+if __name__ == "__main__":
+    building = BuildingFacade()
+    building.enter(1234, "Nancy", 5)
